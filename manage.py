624f0ca49289dbff86ab004ee006ab3d8be5a875
Noah Rogers <nnrogers515@gmail.com>
2021-01-21 00:42:08 +0000

    removing webprocess stuff

--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python
-import os
-import sys
+# #!/usr/bin/env python
+# import os
+# import sys
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coderweb.settings")
+# if __name__ == "__main__":
+#     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coderweb.settings")
 
-    from django.core.management import execute_from_command_line
+#     from django.core.management import execute_from_command_line
 
-    execute_from_command_line(sys.argv)
+#     execute_from_command_line(sys.argv)
